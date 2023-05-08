b77dd9bf739571daaa4e37e411455cd33761d57d
Thong Nguyen <thong.nguyencao@employmenthero.com>
2023-05-08 17:03:51 +0700

    Change the key mapping for nvim-tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,15 @@
-require('dot').use {
-  'folke/which-key.nvim',
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    vim.g.mapleader = ' '
-    require('which-key').setup({})
-    require('which-key').register({
-      e = {'<cmd>NvimTreeFindFileToggle<cr>', 'Toggle Nvim Tree'}
-    }, {
-      prefix = '<leader>'
-    })
-  end
-}
+require("dot").use({
+	"folke/which-key.nvim",
+	config = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+		vim.g.mapleader = " "
+		require("which-key").setup({})
+		require("which-key").register({
+			e = { "<cmd>NvimTreeFindFile<cr>", "Open Nvim Tree" },
+			["`"] = { "<cmd>NvimTreeToggle<cr>", "Toggle Nvim Tree" },
+		}, {
+			prefix = "<leader>",
+		})
+	end,
+})
