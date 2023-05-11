8bc50383f74db3e755e9dbbb2735ec4b2e1b7b2d
Thong Nguyen <thong.nguyencao@employmenthero.com>
2023-05-11 09:48:32 +0700

    Add eslint_d

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -40,6 +40,7 @@ require("dot").use({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.eslint_d,
       },
     })
 
