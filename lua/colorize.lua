a8a8a699bed81166f00ccf8e617e7423aaa5e481
Andrew Nguyen <thong.nguyencao.vn@gmail.com>
2023-05-20 16:45:34 +0700

    Temporary setup autopairs

--- lua/colorize.lua
+++ lua/colorize.lua
@@ -1,12 +1,17 @@
 require("dot").use({
 	"nvim-treesitter/nvim-treesitter",
 	run = ":TSUpdate",
+	requires = { "windwp/nvim-ts-autotag", "windwp/nvim-autopairs" },
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
-      highlight = {
-        enable = true
-      }
+			autotag = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
 		})
+		require("nvim-autopairs").setup({})
 	end,
 })
