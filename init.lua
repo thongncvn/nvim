a01b3ca004ebc05bfb4745512781b99e861b86b8
Andrew Nguyen <thong.nguyencao.vn@gmail.com>
2023-05-21 10:41:05 +0700

    Disable smartindent; it doesn't work

--- init.lua
+++ init.lua
@@ -12,6 +12,8 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.expandtab = true
-vim.o.smartindent = true
+vim.o.smartindent = false
+vim.o.splitbelow = true
+vim.o.splitright = true
 
 vim.g.mapleader = " "
