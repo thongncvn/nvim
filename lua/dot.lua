5a000ffb4bc25a3c3b97280aac0e955eeb546953
Thong Nguyen <thong.nguyencao@employmenthero.com>
2023-05-26 14:20:57 +0700

    refactor: format

--- lua/dot.lua
+++ lua/dot.lua
@@ -1,19 +1,19 @@
 local packages = {}
 
 local function use(package)
-  table.insert(packages, package) 
+	table.insert(packages, package)
 end
 
 local function setup()
-  require('packer').startup(function(use)
-    use('wbthomason/packer.nvim')
-    for _, pocket in ipairs(packages) do
-      use(pocket)
-    end
-  end)
+	require("packer").startup(function(use)
+		use("wbthomason/packer.nvim")
+		for _, pocket in ipairs(packages) do
+			use(pocket)
+		end
+	end)
 end
 
 return {
-  use = use,
-  setup = setup
+	use = use,
+	setup = setup,
 }
