ec72219d4ee9c13d886e031d751da76490fef1df
Thong Nguyen <thong.nguyencao@employmenthero.com>
2023-05-26 14:56:04 +0700

    Update lsp commands; introduce dot#command

--- lua/dot.lua
+++ lua/dot.lua
@@ -13,7 +13,29 @@ local function setup()
 	end)
 end
 
+local function command(name, desc, third_param, fourth_param)
+	local keymap
+	local action
+	local mode
+
+	if type(third_param) == "function" then
+		action = third_param
+		mode = "nokeymap"
+	else
+		keymap = third_param
+		action = fourth_param
+		mode = "keymap"
+	end
+
+	vim.api.nvim_create_user_command(name, action, { desc = desc })
+
+	if mode == "keymap" then
+		vim.keymap.set("n", keymap, "<cmd>" .. name .. "<cr>", { silent = true })
+	end
+end
+
 return {
 	use = use,
 	setup = setup,
+	command = command,
 }
