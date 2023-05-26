c5c99bac505e6970c525ebe643fa888fe057806a
Thong Nguyen <thong.nguyencao@employmenthero.com>
2023-05-26 15:01:21 +0700

    Add ColorschemePicker command

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,12 +1,16 @@
-require('dot').use {
-  'rebelot/kanagawa.nvim',
-  config = function()
-    require('kanagawa').setup {
-      transparent = true,
-      commentStyle = {italic = false},
-      keywordStyle = {italic = false},
-    }
-    vim.cmd [[colorscheme kanagawa]]
-  end
-}
+require("dot").use({
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require("kanagawa").setup({
+			transparent = true,
+			commentStyle = { italic = false },
+			keywordStyle = { italic = false },
+		})
 
+		vim.cmd([[colorscheme kanagawa]])
+	end,
+})
+
+require("dot").command("ColorschemePicker", "Show Telescope colorscheme", function()
+	vim.cmd([[Telescope colorscheme enable_preview=true]])
+end)
