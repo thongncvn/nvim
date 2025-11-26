cd4bb63e984b2b2cbe4c2d7cee2da44bbd4ac359
Thong Nguyen <thong.nguyencao.vn@gmail.com>
2025-11-26 08:25:16 +0000

    Implement fuzzy_searcher using fzf-lua

---
+++ lua/plugins/fuzzy_searcher.lua
@@ -0,0 +1,42 @@
+return {
+  'ibhagwan/fzf-lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('fzf-lua').setup({
+      'default',
+      winopts = {
+        fullscreen = true,
+        preview = {
+          default = 'builtin',
+          border = 'border',
+          wrap = 'nowrap',
+          hidden = 'nohidden',
+          vertical = 'down:45%',
+          horizontal = 'right:60%',
+          layout = 'flex',
+          flip_columns = 120,
+        },
+      },
+      files = {
+        prompt = 'Files❯ ',
+        multiprocess = true,
+        git_icons = true,
+        file_icons = true,
+        color_icons = true,
+      },
+      grep = {
+        prompt = 'Rg❯ ',
+        input_prompt = 'Grep For❯ ',
+        multiprocess = true,
+        git_icons = true,
+        file_icons = true,
+        color_icons = true,
+      },
+    })
+
+    -- Key mappings
+    vim.keymap.set('n', '<leader>p', '<cmd>FzfLua files<cr>', { desc = 'Find files' })
+    vim.keymap.set('n', '<leader>P', '<cmd>FzfLua keymaps<cr>', { desc = 'Keymaps' })
+    vim.keymap.set('n', '<leader>F', '<cmd>FzfLua live_grep<cr>', { desc = 'Live grep' })
+  end,
+}
