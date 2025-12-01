3813bf6aea7bf2be334ce3069f0b26879a0b1b9b
Thong Nguyen <thong.nguyencao.vn@gmail.com>
2025-12-01 06:29:51 +0000

    Make terminal fullscreen

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,6 +7,12 @@ return {
       direction = 'float',
       float_opts = {
         border = 'curved',
+        width = function()
+          return vim.o.columns
+        end,
+        height = function()
+          return vim.o.lines
+        end,
       },
     })
   end,
